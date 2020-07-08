206c69c11a1994b6f7475d73fb81ca7bd57573ff
Катерина Буслова <ekaterinabuslova26@gmail.com>
2020-07-08 10:51:24 +0300

    03.01,02,04

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,7 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!size) {return '';}
+  return string.replace(/1+/g, '$1');
 
 }
